e161aaa4035479b53e5a2023363e27c8bf5e35c4
yupengfei <yupengfei@yupengfeis-MacBook-Pro.local>
2020-02-01 01:18:41 -0800

    second commit

--- Assets/Script/HexCoordinates.cs
+++ Assets/Script/HexCoordinates.cs
@@ -2,24 +2,43 @@ using UnityEngine;
 
 [System.Serializable]
 public struct HexCoordinates {
-    public int X {get; private set;}
+    [SerializeField]
+    private int x, z;
 
-    public int Z {get; private set;}
+    public int X {
+        get {
+            return x;
+        }
+        // private set;
+    }
+
+    public int Z {
+        get {
+            return z;
+        }
+        // private set;
+    }
+
+    public int Y {
+        get {
+            return -this.x - this.z;
+        }
+    }
 
     public HexCoordinates(int x, int z) {
-        X = x;
-        Z = z;
+        this.x = x;
+        this.z = z;
     }
 
     public static HexCoordinates FromOffsetCoordinates(int x, int z) {
-        return new HexCoordinates(x, z);
+        return new HexCoordinates(x - z / 2, z);
     }
 
     public override string ToString() {
-        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+        return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
     }
 
     public string ToStringOnSeparateLines() {
-        return X.ToString() + "\n" + Z.ToString();
+        return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
     }
 }
